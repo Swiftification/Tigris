7921cd3d36abc1cfe8def2a4f3c5b4ea7fef822e
Gabriel Peart <gabriel.peart@me.com>
2016-07-06 14:36:45 +0200

    Adding example of mapping by key

--- Tigris/TigrisTests/AppConfig.swift
+++ Tigris/TigrisTests/AppConfig.swift
@@ -28,11 +28,18 @@ class AppConfig: Config {
     
     func configure() {
         let someUser = UserModel(firstName: "Gabriel", lastName: "Peart")
+        let flashInjectionKey = "flash"
+        let someOtherUser = UserModel(firstName: "Barry", lastName: "Allen")
         injector.map(someUser).asSingleton()
+        injector.map(someOtherUser, key: flashInjectionKey).asSingleton()
         
         // Simulating unmapping
         // injector.unmap(someUser)
         
+        if let flash = injector.get(flashInjectionKey) as? UserModel {
+            print(flash.fullName)
+        }
+
         if let user = injector.get(UserModel) as? UserModel {
             print(user.fullName)
         }
